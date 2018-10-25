e6ee56e4cf2a8e6217159a35c3c4b2f1099fbd52
moskowitza <moskowitzaaron@mac.com>
2018-10-25 15:15:52 -0400

    remove class to revert after played

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("script loaded")
+console.log("script loaded");
 window.addEventListener('keydown',function(e){
     // Don't forget we need quotes around the attribute value
     const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
@@ -8,13 +8,14 @@ window.addEventListener('keydown',function(e){
     if(!audio) return; //if there is nothing to play, stop function
     audio.currentTime=0;//this resets the audio time, see comment below
     audio.play(); //play will not play same element if still playing file
-    key.addClass('playing'); //we'll add a new class for CSS to animate 
+    key.classList.add('playing'); //we'll add a new class for CSS to animate 
 })
 const keys= document.querySelectorAll('.key');
-// loop over each key we're listenign to
+// loop over each key we're listenign to and remove the transform
 function removeTransition(e){
-    if (e.propertyName !== 'transform') return;
-    console.log(e)
+    if (e.propertyName !== 'transform') return; //we are only interested in the transform property
+    // console.log(e);//transitionEvent
+    this.classList.remove('playing');
 }
 
 keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
