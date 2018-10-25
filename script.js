f57168750b8db00a7eb219065a2bfb1fcf1a8745
moskowitza <moskowitzaaron@mac.com>
2018-10-25 16:12:01 -0400

    added click

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 console.log("script loaded");
-window.addEventListener('keydown',function(e){
-    // Don't forget we need quotes around the attribute value
-    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key=document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    console.log(audio)
-    console.log(key)
-    if(!audio) return; //if there is nothing to play, stop function
-    audio.currentTime=0;//this resets the audio time, see comment below
-    audio.play(); //play will not play same element if still playing file
-    key.classList.add('playing'); //we'll add a new class for CSS to animate 
-})
-const keys= document.querySelectorAll('.key');
+function playSound(e) {
+  // Don't forget we need quotes around the attribute value
+  let code;
+  if (e.keyCode) {
+    // it was a keypress, get the keycode as usual
+    code = e.keyCode;
+  } else {
+    // it was a click.  Read the keycode from the div that was clicked
+    code = this.dataset.key;
+  }
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
+  const key = document.querySelector(`.key[data-key="${code}"]`);
+  console.log(audio);
+  console.log(key);
+  if (!audio) return; //if there is nothing to play, stop function
+  audio.currentTime = 0; //this resets the audio time, see comment below
+  audio.play(); //play will not play same element if still playing file
+  key.classList.add("playing"); //we'll add a new class for CSS to animate
+}
+const keys = document.querySelectorAll(".key");
 // loop over each key we're listenign to and remove the transform
-function removeTransition(e){
-    if (e.propertyName !== 'transform') return; //we are only interested in the transform property
-    // console.log(e);//transitionEvent
-    this.classList.remove('playing');
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //we are only interested in the transform property
+  // console.log(e);//transitionEvent
+  this.classList.remove("playing");
 }
 
-keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
+keys.forEach(key => {
+    key.addEventListener("transitionend", removeTransition);
+    key.classList.add("hi")
+    key.addEventListener("click", playSound);
+});
+window.addEventListener("keydown", playSound);
+// document.querySelectorAll('div.key').forEach((key)=>{
+//     key.addEventListener('click',playSound);
+// })
