9599395f846225d8ce24b67629bc254b69fc82e8
moskowitza <moskowitzaaron@mac.com>
2018-12-05 14:56:09 -0500

    layout and working on autocomplete

--- 32_googleLatLng/main.js
+++ 32_googleLatLng/main.js
@@ -1,16 +1,17 @@
-const latInput = document.querySelector("#latInput");
-const lngInput = document.querySelector("#lngInput");
+const latInput = document.querySelector('#latInput');
+const lngInput = document.querySelector('#lngInput');
 function initialize() {
-  const address = document.querySelector("#address");
+  const address = document.querySelector('#address');
   const dropdown = new google.maps.places.Autocomplete(address);
 
-  dropdown.addListener("place_changed", () => {
+  dropdown.addListener('place_changed', () => {
     const place = dropdown.getPlace();
     latInput.value = place.geometry.location.lat();
     lngInput.value = place.geometry.location.lng();
   });
   // if someone hits enter on the address field, don't submit the form
-  input.on("keydown", e => {
+  input.on('keydown', e => {
     if (e.keyCode === 13) e.preventDefault();
   });
 }
+window.onload.initialize();
