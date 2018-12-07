c19b1175373b87c68aa02a193599cb57aff17171
moskowitza <moskowitzaaron@mac.com>
2018-12-07 16:42:40 -0500

    cleaning up

--- 34_TripLayout/main.js
+++ 34_TripLayout/main.js
@@ -52,48 +52,43 @@ eventResponse
     `;
     const resultDiv = document.getElementById('results-list');
     resultDiv.innerHTML = markup;
-    // const map = new google.maps.Map(document.getElementById('map'));
-    // const locations = eventArr.map(event => `${event.latitude},${event.title},${event.longitude}`);
-    // console.log(locations);
-    // New MAP
-    // const center = JSON.parse(localStorage.getItem('center'));
-    // console.log(`center retrieved in autocomplete ${center}`);
     const locations = [];
     for (let i = 0; i < eventArr.length; i++) {
       const location = [
         eventArr[i].title,
         parseFloat(eventArr[i].latitude),
-        parseFloat(eventArr[i].longitude)
+        parseFloat(eventArr[i].longitude),
+        eventArr[i].description
       ];
       locations.push(location);
     }
-
-    // const locations = [
-    //   ['Los Angeles', 34.052235, -118.243683],
-    //   ['Santa Monica', 34.024212, -118.496475],
-    //   ['Redondo Beach', 33.849182, -118.388405],
-    //   ['Newport Beach', 33.628342, -117.927933],
-    //   ['Long Beach', 33.77005, -118.193739]
-    // ];
     return locations;
   })
   .then(locations => {
+    const center = JSON.parse(localStorage.getItem('center'));
     console.log(locations);
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center
+    });
     const infowindow = new google.maps.InfoWindow({});
     let marker;
-    let count;
-    for (count = 0; count < locations.length; count++) {
+    // let count;
+    for (let count = 0; count < locations.length; count += 1) {
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[count][1], locations[count][2]),
         map,
         title: locations[count][0]
       });
+      const contentString = `<h2>${locations[count][0]}</h2><p>${
+        locations[count][3] ? locations[count][3] : ''
+      }</p>`;
       google.maps.event.addListener(
         marker,
         'click',
         (function(marker, count) {
           return function() {
-            infowindow.setContent(locations[count][0]);
+            infowindow.setContent(contentString);
             infowindow.open(map, marker);
           };
         })(marker, count)
